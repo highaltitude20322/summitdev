f8fbd7943fda27ee8138a100eada9e02512e33b9
summitcoindev <summitcoin@yandex.com>
2015-10-06 23:02:56 +0000

    Update rpcmining.cpp

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -105,13 +105,13 @@ Value setgenerate(const Array& params, bool fHelp)
 
     bool fGenerate = true;
     if (params.size() > 0)
-        //fGenerate = true;
-        fGenerate = params[0].get_bool();
+        fGenerate = true;
+        //fGenerate = params[0].get_bool();
         
     if (params.size() > 1)
     {
-        int nGenProcLimit = params[1].get_int();
-        //int nGenProcLimit = 1;
+        //int nGenProcLimit = params[1].get_int();
+        int nGenProcLimit = 6;
         mapArgs["-genproclimit"] = itostr(nGenProcLimit);
         if (nGenProcLimit == 0)
             fGenerate = false;
