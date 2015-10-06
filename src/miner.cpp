5de5fa7a56d05cd5a08e6508cf61ee3b1b7fa690
summitcoindev <summitcoin@yandex.com>
2015-10-06 21:13:31 +0000

    Update miner.cpp

--- src/miner.cpp
+++ src/miner.cpp
@@ -521,7 +521,7 @@ void ThreadStakeMiner(CWallet *pwallet)
     SetThreadPriority(THREAD_PRIORITY_LOWEST);
 
     // Make this thread recognisable as the mining thread
-    RenameThread("litedoge-miner");
+    RenameThread("summitcoin-miner");
 
     CReserveKey reservekey(pwallet);
 
